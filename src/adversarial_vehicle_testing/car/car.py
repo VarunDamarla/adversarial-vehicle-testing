50ea2e51ac72dd1558c7420c6fdab117c8d3700d
VarunDamarla <damarla.varun@gmail.com>
2024-07-17 15:58:11 -0400

    Transfering over to quaternion

--- src/adversarial_vehicle_testing/car/car.py
+++ src/adversarial_vehicle_testing/car/car.py
@@ -25,11 +25,20 @@ model_path = cwd + "/DAVE2v3.pt"
 model = torch.load(model_path, map_location=torch.device("cuda")).eval()
 
 def render_car_translation_pert(x_change: float, y_change: float, z_change: float, device: torch.device) -> str:
+    x_change = torch.tensor(float(x_change), requires_grad=True)
+    x_change.retain_grad()
+    y_change = torch.tensor(float(y_change), requires_grad=True)
+    y_change.retain_grad()
+    z_change = torch.tensor(float(z_change), requires_grad=True)
+    z_change.retain_grad()
+
     mesh = load_objs_as_meshes(["nimrud.obj"], device=device)
     r, t = look_at_view_transform(-20, 170, 50)
-    t[0][0] += x_change
-    t[0][1] += y_change
-    t[0][2] += z_change
+    t[0][0] += x_change.item()
+    t[0][1] += y_change.item()
+    t[0][2] += z_change.item()
+    t.requires_grad = True
+    t.retain_grad()
     cameras = FoVPerspectiveCameras(device=device, R=r, T=t)
     raster_settings = RasterizationSettings(image_size=(225, 400), blur_radius=0.0, faces_per_pixel=1)
     lights = PointLights(device=device, location=[[5.0 , 0.0, 0.0]])
@@ -57,10 +66,10 @@ def render_car_translation_pert(x_change: float, y_change: float, z_change: floa
     plt.figure(figsize=(400, 225))
     plt.imshow(torch.flip(combined[0, ..., :3], [-1]).detach().cpu().numpy())
     plt.axis("off")
-    file_name = f"combined x{x_change}_y{y_change}_z{z_change}.png"
+    file_name = f"combined x{x_change.item()}_y{y_change.item()}_z{z_change.item()}.png"
     cv2.imwrite(file_name, combined[0].detach().cpu().numpy() * 255)
     plt.close()
-    return file_name
+    return file_name, x_change, y_change, z_change, t
 
 def generate_steer_tensor(file_name: str, device: torch.device, model: DAVE2v3) -> torch.Tensor:
     pert_image: torch.Tensor
@@ -78,18 +87,21 @@ def generate_steer_tensor(file_name: str, device: torch.device, model: DAVE2v3) 
 def generate_mlt_translations(n: int, c_x: int, c_y: int, c_z: int, d: torch.device, m:DAVE2v3) -> "list[torch.Tensor]":
     tensors = []
     for i in range(n):
-        results = []
         x = c_x * i
         y = c_y * i
         z = c_z * i
-        results.append(render_car_translation_pert(x, y, z, d))
-        name = f"{cwd}/{results[-1]}"
+        file_name, x_change, y_change, z_change, t = render_car_translation_pert(x, y, z, d)
+        x_change.retain_grad()
+        y_change.retain_grad()
+        z_change.retain_grad()
+        name = f"{cwd}/{file_name}"
         tensor = generate_steer_tensor(name, d, m)
-        tensors.append(tensor)
+        result = [tensor, x_change, y_change, z_change, t]
+        tensors.append(result)
     return tensors
 
 def generate_original_steer() -> torch.Tensor:
-    return generate_steer_tensor(render_car_translation_pert(0, 0, 0, device), device, model)
+    return generate_steer_tensor(cwd + "/straight_scene.jpg", device, model)
 
 def generate_steer_gradient(device: torch.device, model: DAVE2v3, num: int, x:int, y:int, z:int) -> None:
     orig_angle: torch.Tensor
@@ -97,11 +109,16 @@ def generate_steer_gradient(device: torch.device, model: DAVE2v3, num: int, x:in
     data = generate_mlt_translations(num, x, y, z, device, model)
     model.zero_grad()
     losses = []
+    orig_angle.retain_grad()
     for p in range(num):
-        losses.append(-1 * abs(orig_angle - data[p]))
+        data[p][0].retain_grad()
+        data[p][1].retain_grad()
+        data[p][2].retain_grad()
+        data[p][3].retain_grad()
+        losses.append(-1 * abs(orig_angle - data[p][0]))
         print(f"LossP{p}: {losses[-1]}")
         losses[-1].backward(retain_graph=True)
-        print(f"GradientP{p}: {data[p].retain_grad()}")
+        print(f"GradientP{p}: {data[p][1].grad}")
 
 # Understanding how the translation works
 def main() -> None:
